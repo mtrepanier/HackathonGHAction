ccb8503cb9c9f7ba41fce48763a453b612d4cf43
Martin Trepanier <martin.trepanier@autodesk.com>
2020-09-10 14:09:46 -0400

    Fix typo

--- app/models/presence.rb
+++ app/models/presence.rb
@@ -2,5 +2,5 @@ class Presence < ApplicationRecord
   validates :name, :arena, :question1, :question2, :question3, :question4, :presence => {:message => "est requis." }
   validates_format_of :phone_number,
   :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
-  :message => "- Le numero de telephone doit etre du format 555-555-5555."
+  :message => "- Le numéro de téléphone doit être du format 555-555-5555."
 end
