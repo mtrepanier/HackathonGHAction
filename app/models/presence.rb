4541ab7926f2a9a90eea07008b48b6dffe6db56a
Martin Trepanier <martin.trepanier@autodesk.com>
2020-09-19 17:25:17 -0400

    Change format of phone number

--- app/models/presence.rb
+++ app/models/presence.rb
@@ -1,8 +1,5 @@
 class Presence < ApplicationRecord
-  validates :name, :arena, :question1, :question2, :question3, :question4, :start_time, :end_time, :presence => {:message => "est requis." }
-  validates_format_of :phone_number,
-  :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
-  :message => "- Le numéro de téléphone doit être du format 555-555-5555."
+  validates :name, :arena, :phone_number, :question1, :question2, :question3, :question4, :start_time, :end_time, :presence => {:message => "est requis." }
 
   self.per_page = 50
 end
