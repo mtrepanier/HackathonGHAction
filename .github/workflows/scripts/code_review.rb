73375aa66dd129f3a6df2434e830621a14c896a5
Martin Trepanier <martin@trepanier.info>
2023-04-25 10:34:06 -0400

    Functionning code review

--- .github/workflows/scripts/code_review.rb
+++ .github/workflows/scripts/code_review.rb
@@ -3,6 +3,7 @@
 require 'net/http'
 require 'json'
 require 'octokit'
+require 'base64'
 
 class CodeReview
   def initialize(github_token, repository, pull_request_number, chat_gpt_api_key)
@@ -16,48 +17,30 @@ class CodeReview
   def run
     # Get the pull request details
     pull_request = @client.pull_request(@repository, @pull_request_number)
-    head_sha = pull_request.head.sha
 
     # Get the list of files in the pull request
     files = @client.pull_request_files(@repository, @pull_request_number)
-
+    code_review_comments = []
     # For each file, get its contents and generate a code review
     files.each do |file|
       next unless file.status == 'added' || file.status == 'modified'
-
-      file_contents = get_file_contents(file.contents_url)
-      class_name = get_class_name(file.filename)
-      code_review = generate_code_review(class_name, file_contents)
-
-      # Create a review comment for the file
-      create_review_comment(file.filename, code_review, head_sha)
+      code_review = generate_code_review(file.patch)
+      code_review_comments << {comment: code_review, file_name: file.filename}
     end
+
+    create_review_comments(code_review_comments)
   end
 
   private
 
-  def get_file_contents(contents_url)
-    uri = URI.parse(contents_url)
-    request = Net::HTTP::Get.new(uri)
-    request['Authorization'] = "token #{@github_token}"
-    http = Net::HTTP.new(uri.hostname, uri.port)
-    http.use_ssl = true
-    response = http.request(request)
-    JSON.parse(response.body)['content']
-  end
-
-  def get_class_name(filename)
-    File.basename(filename, '.*')
-  end
-
-  def generate_code_review(class_name, file_contents)
-    uri = URI.parse('https://api.openai.com/v1/engines/davinci-codex/completions')
+  def generate_code_review(patch)
+    uri = URI.parse('https://api.openai.com/v1/chat/completions')
     request = Net::HTTP::Post.new(uri)
     request['Authorization'] = "Bearer #{@chat_gpt_api_key}"
     request['Content-Type'] = 'application/json'
     request.body = {
-      prompt: "Review the following code for class #{class_name}:\n\n#{file_contents}",
-      max_tokens: 256,
+      model: "gpt-3.5-turbo",
+      messages: [{"role": "user", "content": "Can you do a code review the following code:\n\n #{patch}"}]
       temperature: 0.5,
       n: 1,
       stop: ['\n']
@@ -65,18 +48,16 @@ class CodeReview
     http = Net::HTTP.new(uri.hostname, uri.port)
     http.use_ssl = true
     response = http.request(request)
-    JSON.parse(response.body)['choices'][0]['text']
+    JSON.parse(response.body)['choices'][0]['message']["content"]
   end
 
-  def create_review_comment(filename, code_review, head_sha)
-    @client.create_pull_request_review(
-      @repository,
-      @pull_request_number,
-      body: "## Code Review for #{filename}\n\n#{code_review}",
-      commit_id: head_sha,
-      path: filename,
-      position: 1
-    )
+  def create_review_comments(code_review_comments)
+    comments = []
+    code_review_comments.each do |code_review_comment|
+      comments << {path: code_review_comment[:file_name], position: 1, body: code_review_comment[:comment]}
+    end
+    options = {event: 'COMMENT', body: "This is close to perfect! Please address the suggested inline change proposed by the ForbiddenFruit.", comments: comments}
+    @client.create_pull_request_review(@repository, @pull_request_number, options)
   end
 end
 
